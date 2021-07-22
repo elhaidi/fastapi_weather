f5421c75688e9555f2a122595b8d1da4c1baf22d
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2021-07-22 16:03:50 +0100

    creating the location model

--- weather/weather_api.py
+++ weather/weather_api.py
@@ -1,8 +1,14 @@
 import fastapi
+from models.location import Location
+from fastapi import Depends
 from typing import Optional
+from pydantic import BaseModel
+
+
 router = fastapi.APIRouter()
+# define a pydantic class to do the validation
 
 
 @router.get("/api/weather/{city}")
-def weather(city: str, state: Optional[str] = None, country: str = 'US', units: Optional[str] = 'metric'):
-    return f"This is some report for {city} in {state} , {country} using {units} "
+def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+    return f"This is some report for {loc.city} in {loc.state} , {loc.country} using {units} "
