00cd4df1deb8b9ef3d5caed8efb4ac4916e71aec
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2021-07-22 10:41:06 +0100

    adding the new css modification

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import fastapi
 import uvicorn
+from starlette.staticfiles import StaticFiles
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 
 
 api = fastapi.FastAPI()
+templates = Jinja2Templates('templates')
+
+api.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 @api.get('/')
-def index():
-    return 'Hello weather'
+def index(request: Request):
+    # the underlining templates need the request object rom starlette
+    return templates.TemplateResponse('index.html', {'request': request})
 
 
 if __name__ == '__main__':
