30a32a113f3323a19a73f14a8a820d3d17c55a25
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2021-07-22 15:01:43 +0100

    adding the first version of the weather api route

--- weather/weather_api.py
+++ weather/weather_api.py
@@ -1,8 +1,8 @@
 import fastapi
-
+from typing import Optional
 router = fastapi.APIRouter()
 
 
 @router.get("/api/weather/{city}")
-def weather(city: str):
-    return "This is some reports" + city
+def weather(city: str, state: Optional[str] = None, country: str = 'US', units: Optional[str] = 'metric'):
+    return f"This is some report for {city} in {state} , {country} using {units} "
