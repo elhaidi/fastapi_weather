d418f8b24a76c11d761f14292650a21ceccac00f
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2021-07-22 23:15:44 +0100

    adding error handling and validation for input data

--- services/weather_service.py
+++ services/weather_service.py
@@ -1,25 +1,58 @@
-""" 
-    this mudule will contain some function to work 
+"""
+    this mudule will contain some function to work
     with  OPEN Weaher API weather API.
 """
 
 import requests
 import httpx
-from typing import Optional
+from typing import Optional, Tuple
+from fastapi import Response
+from models.validation_error import ValidationError
 
 api_key: Optional[int] = None
 
 
 async def get_report_async(city: str, state: Optional[str], country: str, units: str) -> dict:
+
     if state:
         q = f"{city},{state},{country}"
     else:
         q = f"{city},{country}"
     url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}'
     async with httpx.AsyncClient() as client:
-        resp = await client.get(url)
-        resp.raise_for_status()
-
+        resp: Response = await client.get(url)
+        if resp.status_code != 200:
+            raise ValidationError(resp.text, status_code=resp.status_code)
     data = resp.json()
 
     return data['main']
+
+
+def validate_units(city: str, state: Optional[str], country: str, unit: str) -> Tuple[str, Optional[str], str, sr]:
+    # validae the city
+    city = city.lower().strip()
+
+    # validate the country
+    if not country:
+        country = 'us'
+    else:
+        country = country.lower().split()
+    if len(country) != 2:
+        error = f"Invalid country: {country}. It must be a two letter abbreviation such as US or GB."
+        raise ValidationError(error_msg=error, status_code=400)
+
+    # valdiate state:
+    if state:
+        state = state.strip().lower()
+
+    if state and len(state) != 2:
+        error = f"Invalid state: {state}. It must be a two letter abbreviation such as CA or KS (use for US only)."
+        raise ValidationError(status_code=400, error_msg=error)
+    # validate the units
+    if unit:
+        unit = unit.lower().strip()
+    valid_unit = {'metric', 'imperial', 'standard'}
+    if unit not in valid_unit:
+        error = f'Invalid unis {unit}, it must be in {valid_unit}'
+        raise ValidationError(error_msg=error, status_code=400)
+    return city, state, country, unit
--- weather/weather_api.py
+++ weather/weather_api.py
@@ -1,9 +1,11 @@
 import fastapi
+from models.validation_error import ValidationError
 from models.location import Location
 from services import weather_service
 from fastapi import Depends
 from typing import Optional
 from pydantic import BaseModel
+from fastapi import Response
 
 
 router = fastapi.APIRouter()
@@ -12,5 +14,8 @@ router = fastapi.APIRouter()
 
 @router.get("/api/weather/{city}")
 async def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
-    response = await weather_service.get_report_async(loc.city, loc.state, loc.country, units)
+    try:
+        response = await weather_service.get_report_async(loc.city, loc.state, loc.country, units)
+    except ValidationError as err:
+        return Response(content=err.error_msg, status_code=err.status_code)
     return response
